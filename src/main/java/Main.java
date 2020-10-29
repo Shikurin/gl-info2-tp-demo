00e51413d37987c3b5d160fba05f7a129b98db61
Rémi <=>
2020-10-29 11:06:55 +0100

    Modification de Money et création de Main.

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import demo.Money;
+
+class Main {
+
+    public static void main(String[] args) {
+        Money money = new Money(42, "EUR");
+        System.out.println(money);
+    }
+
+}
--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -1,4 +1,25 @@
 package demo;
 
 public class Money {
+
+    int amount;
+    String currency;
+
+    // Constructeur
+    public Money() {
+        this.amount = 0;
+        this.currency = "non renseigné";
+    }
+
+    // Constructeur avec paramètres
+    public Money(int pAmount, String pCurrency) {
+        this.amount = pAmount;
+        this.currency = pCurrency;
+    }
+
+
+    public String toString() {
+        return "Le montant est de " + this.amount + " " + this.currency + ".";
+    }
+
 }
